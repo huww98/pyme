0c5d9469f35d6aee010010e4184a0834a9d37367
胡玮文 <sehuww@mail.scut.edu.cn>
2021-11-29 15:29:42 +0800

    add -O3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ __version__ = "0.0.1"
 
 ext_module = Pybind11Extension("pyme._C",
     ["src/pyme.cpp"],
-    extra_compile_args=['-Wall'],
+    extra_compile_args=['-Wall', '-O3'],
     cxx_std=17,
 )
 
